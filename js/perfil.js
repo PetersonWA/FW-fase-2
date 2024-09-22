172897c27d1ae54bc3d60f5ea0acd2388093e3a9
PetersonWA <pweschenfelder@gmail.com>
2024-09-21 22:32:04 -0300

    hdfhfh
    hdfhdfhdhf

--- js/perfil.js
+++ js/perfil.js
@@ -1,18 +1,39 @@
 // Função para exibir o nome do cliente no perfil
-function exibirNomePerfil() {
-    const nomeCliente = localStorage.getItem('nome');
+    function exibirNomePerfil() {
+        const nomeCliente = localStorage.getItem('nome');
   
-    if (nomeCliente) {
+        if (nomeCliente) {
       document.getElementById('nomePerfil').textContent = nomeCliente;
+        }
     }
-  }
-  
+    // Função para exibir os pets cadastrados
+    function exibirPets() {
+        // Obter a lista de pets do localStorage
+        const pets = JSON.parse(localStorage.getItem('pets')) || [];
+        
+        // Atualizar a exibição dos pets cadastrados
+        //exibirPets();
+    
+        const petList = document.getElementById('petList');
+        petList.innerHTML = '';// Limpar a lista antes de adicionar os novos pets
+      
+        pets.forEach((pet) => {
+          const petInfo = `
+            <strong>Nome:</strong> ${pet.nome} <br>
+            <strong>Raça:</strong> ${pet.raca} <br>
+            <strong>Peso:</strong> ${pet.peso} <br>
+            <strong>Temperamento:</strong> ${pet.temperamento} <hr>
+          `;
+          petList.innerHTML += petInfo;
+        });
+      }
   // Função para cadastrar o pet
   function cadastraPet() {
     const petNome = document.getElementById('petNome').value;
     const petRaca = document.getElementById('petRaca').value;
     const petPeso = document.getElementById('petPeso').value;
     const petTemperamento = document.getElementById('petTemperamento').value;
+
     // Verificar se todos os campos foram preenchidos
     if (petNome && petRaca && petPeso && petTemperamento) {
         // Obter os pets existentes do localStorage (ou inicializar uma array vazia se for a primeira vez)
@@ -22,6 +43,7 @@ function exibirNomePerfil() {
       pets.push(novoPet);
   
       localStorage.setItem('pets', JSON.stringify(pets));
+      // Atualizar a exibição dos pets cadastrados
       exibirPets();
   
       alert('Pet cadastrado com sucesso!');
@@ -30,27 +52,7 @@ function exibirNomePerfil() {
     }
   }
   
-  // Função para exibir os pets cadastrados
-  function exibirPets() {
-    // Obter a lista de pets do localStorage
-    const pets = JSON.parse(localStorage.getItem('pets')) || [];
-    
-    // Atualizar a exibição dos pets cadastrados
-    exibirPets();
 
-    const petList = document.getElementById('petList');
-    petList.innerHTML = '';// Limpar a lista antes de adicionar os novos pets
-  
-    pets.forEach((pet) => {
-      const petInfo = `
-        <strong>Nome:</strong> ${pet.nome} <br>
-        <strong>Raça:</strong> ${pet.raca} <br>
-        <strong>Peso:</strong> ${pet.peso} <br>
-        <strong>Temperamento:</strong> ${pet.temperamento} <hr>
-      `;
-      petList.innerHTML += petInfo;
-    });
-  }
   
   // Carregar os pets cadastrados quando a página for carregada
     document.addEventListener('DOMContentLoaded', exibirPets);
